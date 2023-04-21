6fd77cc36f4ffca9b51b15b68f21ac5b85cce585
Henry Rose <heroseh@protonmail.com>
2023-04-21 07:05:03 +0100

    hcc 0.0.3

--- scripts/build.bat
+++ scripts/build.bat
@@ -26,6 +26,6 @@ IF %EXIT_CODE% NEQ 0 (
 IF "%~1" == "release" (
   echo "=========== Building Release Package ==========="
   cd build
-  powershell Compress-Archive -Force -DestinationPath hcc-0.0.2-windows.zip -LiteralPath hcc.exe, hcc.pdb, ..\libc, ..\libhmaths, ..\libhccintrinsics, ..\interop, ..\samples, ..\playground, ..\docs, ..\README.md, ..\LICENSE
+  powershell Compress-Archive -Force -DestinationPath hcc-0.0.3-windows.zip -LiteralPath hcc.exe, hcc.pdb, ..\libc, ..\libhmaths, ..\libhccintrinsics, ..\interop, ..\samples, ..\playground, ..\docs, ..\README.md, ..\LICENSE
   cd ..
 )
--- src/hcc_main.c
+++ src/hcc_main.c
@@ -235,7 +235,7 @@ int main(int argc, char** argv) {
 			output_final_file = false;
 		} else if (strcmp(argv[arg_idx], "--help") == 0) {
 			printf(
-				"hcc version 0.0.2 help:\n"
+				"hcc version 0.0.3 help:\n"
 				"%s OPTIONS\n"
 				"OPTIONS:\n"
 				"\t-fi   <path>.c          | <path>.c to a C file to compile\n"
